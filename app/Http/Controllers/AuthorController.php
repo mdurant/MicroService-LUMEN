6772b6f1951367932422bad674816e84a36aea57
Mauricio Duran Torres <mauriciodurant@gmail.com>
2018-11-03 22:54:03 -0300

    S crea el Controller d Author vacios, index, store, show, update, destroy

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    //metodos para lister todos, listar autor, crear, editar, eliminar
+
+    /**
+     * Retorna la lista de autores
+     */
+      public function index(){
+
+      }
+      /**
+     * Crea la Instancia de autores
+     */
+      public function store(Request $request){
+
+      }
+      /**
+     * Mustra la Información de 1 author
+     */
+      public function show($author){
+
+      }
+      /**
+     * Actualizar autores
+     */
+      public function update(Request $request, $author){
+
+      }
+    /**
+     * Elimina autores
+     */
+      public function destroy($author){
+
+      }
+
+    //
+}
